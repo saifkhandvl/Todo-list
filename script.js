2d4ff41b3f7e04f7e28a367607338e448b4cb040
saif <saifs252525@gmail.com>
2020-12-14 23:49:58 -0800

    Procfile and modified

--- script.js
+++ script.js
@@ -165,7 +165,12 @@ app.get("/about",function(req,res){
 
 })
 
+let port=process.env.PORT;
+if(port==null || port==""){
+	port=3000;
+}
 
-app.listen(3000,function(){
-	console.log("srever is runing 27017");
+
+app.listen(port,function(){
+	console.log("srever is runing 3000");
 })
